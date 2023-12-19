6f0b86bccb053d7bda76e0b26cb2b826baa9cd62
ikeyu0806 <ikeyu0806@gmail.com>
2023-12-19 15:32:09 +0900

    install actix-web

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM rust:latest
 
+WORKDIR /app
+
 RUN apt-get update \
   && apt-get upgrade -y \
   && apt-get install -y -q
+
+CMD ["cargo", "run"]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-  println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(index).service(greet)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
