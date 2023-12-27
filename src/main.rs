fb6ab235d7cf27c1ff639cbb76735701d63ac986
ikeyu0806 <ikeyu0806@gmail.com>
2023-12-27 10:57:10 +0900

    create util mod

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,9 @@ use actix_web::{post, web, App, HttpResponse, HttpServer};
 use futures::StreamExt;
 use serde::{Deserialize, Serialize};
 use rusqlite::{params, Connection, Result};
-use std::fmt;
 
-#[derive(Debug)]
-struct CustomError {
-    message: String,
-}
-
-impl actix_web::error::ResponseError for CustomError {
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::InternalServerError().body(self.message.clone())
-    }
-}
-
-impl fmt::Display for CustomError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl From<actix_web::error::PayloadError> for CustomError {
-    fn from(_: actix_web::error::PayloadError) -> Self {
-        CustomError {
-            message: "Payload error".to_string(),
-        }
-    }
-}
+mod util;
+use util::CustomError;
 
 #[derive(Serialize, Deserialize)]
 struct Todo {
---
+++ src/util/mod.rs
@@ -0,0 +1,27 @@
+use actix_web::HttpResponse;
+use std::fmt;
+
+#[derive(Debug)]
+pub struct CustomError {
+    pub message: String,
+}
+
+impl actix_web::error::ResponseError for CustomError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::InternalServerError().body(self.message.clone())
+    }
+}
+
+impl fmt::Display for CustomError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl From<actix_web::error::PayloadError> for CustomError {
+    fn from(_: actix_web::error::PayloadError) -> Self {
+        CustomError {
+            message: "Payload error".to_string(),
+        }
+    }
+}
