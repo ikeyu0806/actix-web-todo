5b41e68949209a38671fc8eec4e121b423afd5b6
ikeyu0806 <ikeyu0806@gmail.com>
2023-12-27 11:40:49 +0900

    create infrasttructure module

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,20 @@
 use actix_web::{post, web, App, HttpResponse, HttpServer};
 use futures::StreamExt;
 use serde::{Deserialize, Serialize};
-use rusqlite::{params, Connection, Result};
+use rusqlite::{params};
+
 
 mod util;
 use util::CustomError;
+
+mod infrastructure;
+use infrastructure::init_db;
 
 #[derive(Serialize, Deserialize)]
 struct Todo {
     id: Option<i64>,
     title: String,
     contents: String,
-}
-
-fn init_db() -> Result<Connection, CustomError> {
-    let conn = Connection::open_in_memory().map_err(|_| CustomError {
-        message: "Failed to initialize the database.".to_string(),
-    })?;
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS todos (
-            id INTEGER PRIMARY KEY,
-            title TEXT NOT NULL,
-            contents TEXT NOT NULL
-        )",
-        [],
-    )
-    .map_err(|_| CustomError {
-        message: "Failed to create todos table.".to_string(),
-    })?;
-
-    Ok(conn)
 }
 
 #[post("/todos")]
