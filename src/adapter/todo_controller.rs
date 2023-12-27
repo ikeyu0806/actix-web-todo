ad937f5ebae64fe9e61eec9d1d0b343b77d1e995
ikeyu0806 <ikeyu0806@gmail.com>
2023-12-27 14:34:16 +0900

    create adapter module

---
+++ src/adapter/todo_controller.rs
@@ -0,0 +1,36 @@
+use actix_web::{post, web, HttpResponse};
+use futures::StreamExt;
+use rusqlite::{params};
+
+use super::super::util::error::CustomError;
+use super::super::domain::entity::Todo;
+use super::super::infrastructure::sqlite::init_db;
+
+#[post("/todos")]
+pub async fn create_todo(mut payload: web::Payload) -> Result<HttpResponse, CustomError> {
+  let mut body = web::BytesMut::new();
+  while let Some(chunk) = payload.next().await {
+    let chunk = match chunk {
+      Ok(chunk) => chunk,
+      Err(err) => return Err(err.into()),
+    };
+
+    body.extend_from_slice(&chunk);
+  }
+
+  let todo = serde_json::from_slice::<Todo>(&body)
+    .map_err(|err| CustomError {
+       message: format!("{}", err),
+    })?;
+
+  let conn = init_db()?;
+
+  conn.execute(
+    "INSERT INTO todos (title, contents) VALUES (?1, ?2)",
+    params![&todo.title, &todo.contents],
+  ).map_err(|err| CustomError {
+    message: format!("Failed to insert todo into the database: {}", err),
+  })?;
+
+  Ok(HttpResponse::Ok().json(todo))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,18 @@
-use actix_web::{post, web, App, HttpResponse, HttpServer};
-use futures::StreamExt;
-use rusqlite::{params};
+use actix_web::{App, HttpServer};
 
+mod adapter;
+use adapter::todo_controller::create_todo;
 mod domain;
-use domain::entity::Todo;
-
 mod infrastructure;
-use infrastructure::sqlite::init_db;
-
 mod util;
-use util::error::CustomError;
-
-#[post("/todos")]
-async fn create_todo(mut payload: web::Payload) -> Result<HttpResponse, CustomError> {
-    let mut body = web::BytesMut::new();
-    while let Some(chunk) = payload.next().await {
-        let chunk = match chunk {
-            Ok(chunk) => chunk,
-            Err(err) => return Err(err.into()),
-        };
-    
-        body.extend_from_slice(&chunk);
-    }
-
-    let todo = serde_json::from_slice::<Todo>(&body)
-        .map_err(|err| CustomError {
-            message: format!("{}", err),
-        })?;
-
-    let conn = init_db()?;
-
-    conn.execute(
-        "INSERT INTO todos (title, contents) VALUES (?1, ?2)",
-        params![&todo.title, &todo.contents],
-    ).map_err(|err| CustomError {
-        message: format!("Failed to insert todo into the database: {}", err),
-    })?;
-
-    Ok(HttpResponse::Ok().json(todo))
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(create_todo)
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+  HttpServer::new(|| {
+    App::new()
+        .service(create_todo)
+  })
+  .bind(("0.0.0.0", 8080))?
+  .run()
+  .await
 }
